f74c866c05f5d483e772814acc19b40ea44a6b93
ED-IP <edip_courses@outlook.es>
2021-04-19 16:45:34 +0000

    fixed last name field in request information form not beign sent

--- js/emailJS.js
+++ js/emailJS.js
@@ -1,7 +1,7 @@
 function sendEmail(requestForm) {
     emailjs.send("service_yavk8t6", "template_3d8n1ip", {
             "from_name": requestForm.name.value,
-            "lastname": requestForm.last - name.value,
+            "lastname": requestForm.lname.value,
             "from_email": requestForm.email.value,
             "information_request": requestForm.information.value
         })
