6335f37c5e1aab3875307ad51bddd938caaec2b9
ED-IP <edip_courses@outlook.es>
2021-04-19 11:15:44 +0000

    final code for the emailJS integration

--- js/emailJS.js
+++ js/emailJS.js
@@ -1,9 +1,17 @@
-function sendEmail(requestForm){
-    emailjs.send(service_yavk8t6, template_3d8n1ip{
-        "from_name":requestForm.name.value,
-        "lastname":requestForm.last-name.value,
-        "from_email":requestForm.email.value,
-        "information_request":requestForm.information.value
-    })
-    .then()
+function sendEmail(requestForm) {
+    emailjs.send(service_yavk8t6, template_3d8n1ip, {
+            "from_name": requestForm.name.value,
+            "lastname": requestForm.last - name.value,
+            "from_email": requestForm.email.value,
+            "information_request": requestForm.information.value
+        })
+        .then(
+            function (response) {
+                console.log(SUCCESS, response);
+            },
+
+            function (error) {
+                console.log(FAILED, error);
+            });
+    return false;
 }
