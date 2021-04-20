31cc3fd6de79c69f788bbd29d5ba79e5259fcd9f
ED-IP <edip_courses@outlook.es>
2021-04-20 12:04:45 +0000

    added js code to open/close provacy policy window

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Code to make the icons of the Activity section bigger and recover their initial size when the mouse hover over the text on the column
+
 $(".img-col-text").hover(function () {
         $(this).siblings().first().css("color", "rgb(220, 3, 3)").css("font-size", "55px").css("border", "none");
     },
@@ -5,9 +7,21 @@ $(".img-col-text").hover(function () {
         $(this).siblings().first().css("color", "black").css("font-size", "35px").css("border", "2px solid");
     });
 
+// Code to make the images of the Activity section sighly bigger and recover their initial size when the mouse hover over them
+
 $(".img-col").hover(function () {
         $(this).css("width", "110%").css("height", "505px");
     },
     function () {
         $(this).css("width", "100%").css("height", "500px");
     });
+
+// Code to open/close the Privacy policy window, found it here https://stackoverflow.com/a/24659694
+
+function openWin() {
+    myWindow = window.open("privacy_policy.html", "_blank", "width=500, height=800");
+}
+
+function closeWin(){
+    window.close();
+}
