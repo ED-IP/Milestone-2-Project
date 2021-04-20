b0eb58bba18025490d7a0375b619e523f57d1835
ED-IP <edip_courses@outlook.es>
2021-04-20 09:34:55 +0000

    added code to scale images and icons in the activity section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
 $(".img-col-text").hover(function(){
-    $(this).siblings().first().css("color", "orange").css("font-size", "55px").css("width: 100px;").css("height: 100px;").css("border", "none");
+    $(this).siblings().first().css("color", "rgb(220, 3, 3)").css("font-size", "55px").css("border", "none");
     },
     function(){
-        $(this).siblings().first().css("color", "black").css("font-size", "35px").css("width", "70px").css("height", "70px").css("border", "2px solid");
+        $(this).siblings().first().css("color", "black").css("font-size", "35px").css("border", "2px solid");
+     });
+
+ $(".img-col").hover(function(){
+     $(this).css("width", "110%").css("height", "505px");
+    },
+    function(){
+      $(this).css("width", "100%").css("height", "500px");
  });
     
