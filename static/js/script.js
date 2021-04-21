e8e5591ca353f23fbcfbd06a712789e9c8c51108
ED-IP <edip_courses@outlook.es>
2021-04-21 16:36:44 +0000

    changed the height for the .img-col-text class

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-// Code to make the icons of the Activity section bigger and recover their initial size when the mouse hover over the text on the column
+/** Code to make the icons of the Activity section bigger and recover their initial size when the mouse hover over the text on the column */
 
 $(".img-col-text").hover(function () {
         $(this).siblings().first().css("color", "rgb(220, 3, 3)").css("font-size", "55px").css("border", "none");
@@ -7,7 +7,7 @@ $(".img-col-text").hover(function () {
         $(this).siblings().first().css("color", "black").css("font-size", "35px").css("border", "2px solid");
     });
 
-// Code to make the images of the Activity section sighly bigger and recover their initial size when the mouse hover over them
+/** Code to make the images of the Activity section sighly bigger and recover their initial size when the mouse hover over them */
 
 $(".img-col").hover(function () {
         $(this).css("width", "110%").css("height", "505px");
